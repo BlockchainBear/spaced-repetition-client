a4b1701d9c8e9b2c0ab4c9e21f381f345ef15dd0
Adam Figueroa <admiralfiggins@gmail.com>
2021-02-24 15:27:48 -0800

    finished styling for login/register page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,57 +1,56 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import TokenService from '../../services/token-service'
-import UserContext from '../../contexts/UserContext'
-import './Header.css'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import TokenService from '../../services/token-service';
+import UserContext from '../../contexts/UserContext';
+import Logo from '../../images/rocket-logo.png';
+import './Header.css';
 
 class Header extends Component {
-  static contextType = UserContext
+  static contextType = UserContext;
 
   handleLogoutClick = () => {
-    this.context.processLogout()
-  }
+    this.context.processLogout();
+  };
 
   renderLogoutLink() {
     return (
       <div>
-        <span>
-          {this.context.user.name}
-        </span>
-        <nav>
-          <Link
-            onClick={this.handleLogoutClick}
-            to='/login'>
+        <span>{this.context.user.name}</span>
+        <nav className="logoutNav">
+          <Link onClick={this.handleLogoutClick} to="/login">
             Logout
           </Link>
         </nav>
       </div>
-    )
+    );
   }
 
   renderLoginLink() {
     return (
-      <nav>
-        <Link to='/login'>Login</Link>
-        {' '}
-        <Link to='/register'>Sign up</Link>
+      <nav className="loginNav">
+        <div className='loginLink'><Link to="/login">Login</Link></div>
+        <div className='signupLink'><Link to="/register">Sign up</Link></div>
       </nav>
-    )
+    );
   }
 
   render() {
     return (
-      <header>
-        <h1>
-          <Link to='/'>
-            Spaced repetition
-          </Link>
-        </h1>
-        {TokenService.hasAuthToken()
-          ? this.renderLogoutLink()
-          : this.renderLoginLink()}
+      <header className="headerB">
+        <div className="navBox">
+          <div className="logoBox">
+            <h1 className="logo">
+              <Link to="/">Spaced Repetition</Link>
+            </h1>
+            <img className="logoIcon" src={Logo} alt="Rocket Logo" />
+          </div>
+          {TokenService.hasAuthToken()
+            ? this.renderLogoutLink()
+            : this.renderLoginLink()}
+        </div>
       </header>
     );
   }
 }
 
-export default Header
+export default Header;
